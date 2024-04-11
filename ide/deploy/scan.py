1538c2758f1d801a1a4127c9b11e29b46e404623
i-mattanasio <i-mattanasio@codarchitects.com>
2024-04-11 15:37:32 +0200

    fix: scanner excludes the correct files

--- ide/deploy/scan.py
+++ ide/deploy/scan.py
@@ -15,38 +15,81 @@
 # specific language governing permissions and limitations
 # under the License.
 
+from colorama import Fore, Style
 from glob import glob
 from .deploy import *
+import json
 
 def scan():
+
     # first look for requirements.txt and build the venv (add in set)
     deployments = set()
     packages = set()
 
-    print(">>> Scan:")
+    if not os.path.exists("actions.json"):
+        return
+
+    data = json.loads(open("actions.json").read())
+    excluded = data["excluded"]
+
     reqs =  glob("packages/*/*/requirements.txt") + glob("packages/*/*/package.json")
-    # req = reqs[0]
-    # from util.deploy.deploy import *
+    print("========== REQUIREMENT FILES ========== \n")
     for req in reqs:
-        print(">", req)
+        _req = req.rsplit("/",1)[0]
+        skip = False # Used to skip a request if matched in the excluded list
+
+        for excl in excluded:
+            if _req.startswith(excl):
+                skip = True
+                print(f">> {req} is {Fore.RED}excluded{Style.RESET_ALL}, skipping update.")
+                break
+
+        if skip:
+            continue # Skip req
+
+        print(f">> {_req} is {Fore.LIGHTGREEN_EX}included{Style.RESET_ALL}, updating.")    
         sp = req.split("/")
         act = build_zip(sp[1],sp[2])
         deployments.add(act)
         packages.add(sp[1])
-        
+
+
     mains = glob("packages/*/*/index.js") + glob("packages/*/*/__main__.py")
-    # main = mains[2]
+    print("========== MAIN FILES ========== \n")
     for main in mains: 
-        print(">", main)
+        skip = False
+
+        for excl in excluded:
+            if main.startswith(excl):
+                skip = True
+                print(f"> {main} is {Fore.RED}excluded{Style.RESET_ALL}, skipping update.")
+                break
+
+        if skip:
+            continue # Skip main
+
+        print(f"> {main} is {Fore.LIGHTGREEN_EX}included{Style.RESET_ALL}, updating.")    
         sp = main.split("/")
         act = build_action(sp[1],sp[2])
         deployments.add(act)
-        packages.add(sp[1])    
+        packages.add(sp[1]) 
+    
 
     singles = glob("packages/*/*.py") +  glob("packages/*/*.js")
-    # single = singles[0]
+    print("========== SINGLE FILES ========== \n")
     for single in singles:
-        print(">", single)
+        skip = False
+
+        for excl in excluded:
+            if single.startswith(excl):
+                skip = True
+                print(f">> {single} is {Fore.RED}excluded{Style.RESET_ALL}, skipping update.")
+                break
+        
+        if skip: 
+            continue # Skip single file
+    
+        print(f">> {single} is {Fore.LIGHTGREEN_EX}included{Style.RESET_ALL}, updating.")    
         sp = single.split("/")
         deployments.add(single)
         packages.add(sp[1])
@@ -60,3 +103,4 @@ def scan():
     for action in deployments:
         print("^", action)
         deploy_action(action)
+
